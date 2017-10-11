7ec832d2cf8063eb7cc6ba907e8f1b96e19861e7
Facundo <som@som.com.ar>
2017-10-11 11:41:59 -0300

    Cambio cámara por Galería.

--- app/src/main/java/com/som/som/Fotos.java
+++ app/src/main/java/com/som/som/Fotos.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
