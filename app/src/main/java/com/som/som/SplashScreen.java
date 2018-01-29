324d804f55287e672986f9b13fd3c089d26f6867
Facundo <som@som.com.ar>
2018-01-29 11:39:22 -0300

    Splash Screen, modificaciones.

--- app/src/main/java/com/som/som/SplashScreen.java
+++ app/src/main/java/com/som/som/SplashScreen.java
@@ -43,7 +43,7 @@ public class SplashScreen extends AppCompatActivity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 2000) {
+                    while (waited < 1500) {
                         sleep(100);
                         waited += 100;
                     }
